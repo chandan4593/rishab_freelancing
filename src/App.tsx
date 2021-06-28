5f6ddfdb1e0f46daee8c20927da756caeb5f1777
DeepeshDragoneel <deepeshash444@gmail.com>
2021-06-28 11:21:37 +0530

    shop

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {Switch,Route} from "react-router-dom";
 import Welcome from "./Components/Welcome/Welcome";
+import Shop from './Components/Shop/Shop';
 function App() {
   return (
-    <div className="App">
-      <Switch>
-        <Route exact path="/" component={Welcome}></Route>  
-      </Switch>
-    </div>
+      <div className="App">
+          <Switch>
+              <Route exact path="/" component={Shop}></Route>
+          </Switch>
+      </div>
   );
 }
 export default App;
---
+++ src/Components/Shop/Shop.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Shop.scss";
+import ShopNavbar
+    from '../ShopNavbar/ShopNavbar';
+
+const Shop = () => {
+    return (
+        <div>
+            <ShopNavbar/>
+            <h1>KISAN!</h1>
+        </div>
+    )
+}
+
+export default Shop;
