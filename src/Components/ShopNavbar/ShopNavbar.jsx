d591cf0739cda56dddfbc5ae46660e2a99b81ec2
DeepeshDragoneel <deepeshash444@gmail.com>
2021-06-28 19:19:19 +0530

    Added orders

--- src/Components/ShopNavbar/ShopNavbar.jsx
+++ src/Components/ShopNavbar/ShopNavbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./ShopNavbar.scss";
-import logo from "../assets/logo.jpeg"
+import logo from "../assets/logo.jpeg";
 import {
     Collapse,
     Navbar,
@@ -15,26 +15,76 @@ import {
     DropdownItem,
     NavbarText,
 } from "reactstrap";
+import { useHistory } from "react-router-dom";
 
 const ShopNavbar = (props) => {
     const [isOpen, setIsOpen] = useState(false);
 
     const toggle = () => setIsOpen(!isOpen);
 
+    const history = useHistory();
+
     return (
         <div>
-            <Navbar light expand="md" style={{ backgroundColor:"white" ,position: "fixed" , width:"100%", zIndex: "100"}}>
-                <NavbarBrand href="/"><img src={logo} alt="logo" style={{
-                    width: "6rem", height: "3rem"
-                }}></img></NavbarBrand>
+            <Navbar
+                light
+                expand="md"
+                style={{
+                    backgroundColor: "white",
+                    position: "fixed",
+                    width: "100%",
+                    zIndex: "100",
+                }}
+            >
+                <NavbarBrand href="/">
+                    <img
+                        src={logo}
+                        alt="logo"
+                        style={{
+                            width: "6rem",
+                            height: "3rem",
+                        }}
+                    ></img>
+                </NavbarBrand>
                 <NavbarToggler onClick={toggle} />
                 <Collapse isOpen={isOpen} navbar>
                     <Nav className="ml-auto" navbar>
                         <NavItem>
-                            <NavLink href="/components/">Pending Orders</NavLink>
+                            <NavLink
+                                onClick={() => {
+                                    history.push("../shop");
+                                }}
+                                style={{
+                                    cursor: "pointer",
+                                }}
+                                activeClassName="linkIsActive"
+                            >
+                                Shop
+                            </NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink href="https://github.com/reactstrap/reactstrap">
+                            <NavLink
+                                onClick={() => {
+                                    history.push("../pendingOrders");
+                                }}
+                                style={{
+                                    cursor: "pointer",
+                                }}
+                                activeClassName="linkIsActive"
+                            >
+                                Pending Orders
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink
+                                onClick={() => {
+                                    history.push("../acceptedOrders");
+                                }}
+                                style={{
+                                    cursor: "pointer",
+                                }}
+                                activeClassName="linkIsActive"
+                            >
                                 Orders
                             </NavLink>
                         </NavItem>
